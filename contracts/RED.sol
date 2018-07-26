b22a8faa18c9d8c36b464203e09a2f18ae28523d
dung <chinhdung180@gmail.com>
2018-07-26 11:43:25 +0700

    update smartcontract

--- contracts/RED.sol
+++ contracts/RED.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.23;
 // contract address: 
 // hash: https://packagist.org/packages/hashing/keccak256
-//https://github.com/raineorshine/solidity-by-example/blob/master/remove-from-array.sol
+// https://github.com/raineorshine/solidity-by-example/blob/master/remove-from-array.sol
 
 contract Owned {
     address owner;
@@ -57,15 +57,23 @@ contract RED is Owned {
         return (contracts[_contract_id].created_at, contracts[_contract_id].hash_value);
     }
     
-    //function checkDocumentHash(bytes32 _hash) view public returns(bool){
-    //    uint i = 0;
-    //    while (keccak256(contracts[i].hash_value) != keccak256(_hash)) {
-    //        i++;
-    //    }
-    //}
+    function checkDocumentHash(bytes32 _hash) view public returns(bool){
+        uint i = 0;
+        while (contracts[i].hash_value != _hash) {
+            i++;
+        }
+        
+        //not exist hash value
+        if(i==0){
+            return false;
+        }
+        //exist hash value
+        return true;
+        
+    }
     
-    function checkDocumentContractID(uint _contract_id, bytes32 _hash) view public returns(bool){
-        if(keccak256(contracts[_contract_id].hash_value) != keccak256(_hash)){
+    function checkExistDocument(uint _contract_id, bytes32 _hash) view public returns(bool){
+        if(contracts[_contract_id].hash_value == _hash){
             return true;
         }
         return false;
